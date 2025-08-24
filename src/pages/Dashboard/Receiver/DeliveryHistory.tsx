1ebbfa53576cd523ec1a9d89c46aa5d200225fc7
bodruddozaredoy <bodruddozaredoy@gmail.com>
2025-08-25 03:42:52 +0600

    Added: delivery history page and functions

--- src/pages/Dashboard/Receiver/DeliveryHistory.tsx
+++ src/pages/Dashboard/Receiver/DeliveryHistory.tsx
@@ -1,7 +1,113 @@
-import React from 'react'
+import { Badge } from '@/components/ui/badge'
+import type { Parcel } from '@/types/index.types'
+import { useCancelParcelMutation, useGetAllParcelsQuery, useGetIncomingParcelsQuery, useGetMyParcelsQuery, useToggleParcelBlockMutation, useUpdateParcelStatusMutation } from '@/redux/features/parcel/parcel.api'
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+import { Button } from '@/components/ui/button'
+// import AddParcelModal from './AddParcelModal'
+import { toast } from 'sonner'
+
 
 export default function DeliveryHistory() {
-  return (
-    <div>DeliveryHistory</div>
-  )
+
+    const { data: parcels } = useGetAllParcelsQuery(undefined)
+    const [toggleBlock] = useToggleParcelBlockMutation()
+    const [updateParcelStatus] = useUpdateParcelStatusMutation()
+    console.log("parcels", parcels?.data)
+
+    const updatedParcels = parcels?.data?.filter((prev: any) => prev.currentStatus === "delivered")
+
+    const handleBlockParcel = async (parcelId: string, isBlocked: boolean) => {
+        try {
+            const res = await toggleBlock(parcelId).unwrap()
+            console.log(res)
+            if (!isBlocked) {
+                toast.success("Blocked the parcel!")
+            } else {
+                toast.success("Unblocked the parcel!")
+            }
+        } catch (error) {
+            toast.error("Couldn't cancel the parcel!")
+        }
+    }
+
+    const handleUpdateParcelStatus = async (parcelId: string, status: string) => {
+        const data = {
+            status: status,
+            location: "murpur",
+            note: "muri kha"
+        }
+        try {
+            const res = await updateParcelStatus({ parcelId, data })
+            console.log(res)
+            toast.success("Status updated!")
+        } catch (error) {
+            toast.error("Something went wrong!")
+        }
+    }
+
+    return (
+        <div className='p-5'>
+            <div className='mb-3'>
+                {/* <AddParcelModal /> */}
+            </div>
+            <div className='border rounded-lg overflow-hidden p-5'>
+                <Table>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead>Tracking ID</TableHead>
+                            <TableHead>Type</TableHead>
+                            <TableHead>Weight</TableHead>
+                            <TableHead>Fee</TableHead>
+                            {/* <TableHead>Sender</TableHead> */}
+                            {/* <TableHead>Receiver</TableHead> */}
+                            <TableHead>From</TableHead>
+                            <TableHead>To</TableHead>
+                            <TableHead>Status</TableHead>
+                            {/* <TableHead>Delivery Date</TableHead> */}
+                            <TableHead>Created At</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {
+                            updatedParcels?.map((parcel: Parcel, index: number) => (
+                                <TableRow key={index}>
+                                    <TableCell className="">{parcel.trackingId}</TableCell>
+                                    <TableCell>{parcel.type}</TableCell>
+                                    <TableCell>{parcel?.weight}Kg</TableCell>
+                                    <TableCell>${parcel.fee}</TableCell>
+                                    <TableCell>{parcel.fromAddress}</TableCell>
+                                    <TableCell>{parcel.toAddress}</TableCell>
+                                    <TableCell>
+                                        <Badge variant="outline">{parcel?.currentStatus?.toLocaleUpperCase()}</Badge>
+                                    </TableCell>
+                                    <TableCell>{parcel.createdAt}</TableCell>
+
+
+                                </TableRow>
+                            ))
+                        }
+
+
+                    </TableBody>
+                </Table>
+
+            </div>
+        </div>
+    )
 }
